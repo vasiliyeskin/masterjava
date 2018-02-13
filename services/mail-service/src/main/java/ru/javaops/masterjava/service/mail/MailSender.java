8d324fe20b0d3d3601f3412ad4dbe4567b0cbc63
vasiliyeskin <eskin@rf.unn.ru>
2018-02-13 11:39:18 -0600

    HW06 create initial version

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,55 @@
 package ru.javaops.masterjava.service.mail;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+
 
 @Slf4j
 public class MailSender {
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
         log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.yandex.ru");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+            email.setSSLOnConnect(true);
+            email.setFrom("user@yandex.ru");
+            email.setSubject("TestMail");
+            email.setMsg("This is a test mail ... :-)");
+            email.addTo("foo@bar.com");
+            email.send();
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+
+
+
+ /*       // Create the attachment
+        EmailAttachment attachment = new EmailAttachment();
+        attachment.setURL(new URL("http://www.apache.org/images/asf_logo_wide.gif"));
+        attachment.setDisposition(EmailAttachment.ATTACHMENT);
+        attachment.setDescription("Apache logo");
+        attachment.setName("Apache logo");
+
+        // Create the email message
+        MultiPartEmail email = new MultiPartEmail();
+        email.setHostName("mail.myserver.com");
+        email.addTo("jdoe@somewhere.org", "John Doe");
+        email.setFrom("me@apache.org", "Me");
+        email.setSubject("The logo");
+        email.setMsg("Here is Apache's logo");
+
+        // add the attachment
+        email.attach(attachment);
+
+        // send the email
+        email.send();*/
+
     }
 }
